0422fb1e518fb6ec64258d2dd8eaed4baa78f6b0
kotaro.watanabe <kotaro.watanabe@cinc-j.co.jp>
2021-08-17 15:05:15 +0900

    add code

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,3 +1,22 @@
+from djangosnippets import snippets
+from django.urls import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
-# Create your tests here.
+from snippets.views import top, snippet_new, snippet_detail, snippet_edit
+
+class TopPageTest(TestCase):
+    def test_top_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b'Hello World')
+
+class CreateSnippetTest(TestCase):
+    def test_should_resolve_snippet_new(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new, found.func)
+
+
